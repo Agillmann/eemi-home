2c9a3f2f38cac284d749cbffdd9c49006e4c3e34
Gillmann <adrien.gillmann@gmail.com>
2020-01-08 17:34:46 +0100

    test qrcode generate

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import Card from '../Cards/Card';
+import React from "react";
+import QRCode from "qrcode.react";
+import Card from "../Cards/Card";
 
 const HomePage = () => {
   return (
     <div className="">
       <h1>Home Page</h1>
+      <QRCode value="https://maps.google.com/?ll=48.868759,2.3409" />,
       <Card />
     </div>
   );
